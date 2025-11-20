fabadb8b79a1817aa996af75488a1f806e8b3ec2
Yout Name <gianmaria.casamenti@studio.unibo.it>
2025-11-20 11:12:38 +0100

    feat: flask api creation

---
+++ src/backend/app/__init__.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, send_from_directory
+from flask_cors import CORS
+from .services import mongodb
+from .routes import assignments
+from .routes import submissions
+
+def create_app():
+    """
+    Flask app factory, create application
+    """
+    app = Flask(__name__)
+    CORS(app)
+
+    mongo_uri = os.environ.get('MONGO_URI')
+    try:
+        mongodb.init_db(mongo_uri)
+        print("MongoDB connected with successfully")
+    except Exception as e:
+        print(f"MongoDB connection error: {e}")
+
+    app.register_blueprint(assignments.assignments_bp)
+    app.register_blueprint(submissions.submissions_bp)
+
+    @app.route('/')
+    def base_endpoint():
+        """Base endpoint for testing purposes (DB connection)) ."""
+        db_status = mongodb.check_db_connection()
+        return {
+            "message": "Flask server is alive.",
+            "database_status": db_status
+        }
+
+    return app
