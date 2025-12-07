c52cacfe74beb0e7590f509bf0b5caf20be3141e
Yout Name <gianmaria.casamenti@studio.unibo.it>
2025-12-07 17:59:22 +0100

    chore: change import

--- benchmark/tests/fault_tolerance.py
+++ benchmark/tests/fault_tolerance.py
@@ -4,11 +4,10 @@ import threading
 import sys
 import os
 import json
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
-import config
+import benchmark.config as config
 from drivers.swarm_driver import SwarmDriver
 
 stop_traffic = False
