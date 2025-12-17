d85a6d6a97eca0999a87139cd3a6acc26b499d21
Yout Name <gianmaria.casamenti@studio.unibo.it>
2025-12-17 16:09:02 +0100

    fix: change names of manifest and

--- benchmark/config.py
+++ benchmark/config.py
@@ -1,5 +1,5 @@
-# Ora puntiamo alla porta 80 e usiamo il prefisso /api per i test del backend.yaml
+# Ora puntiamo alla porta 80 e usiamo il prefisso /api per i test del 03-backend.yaml
 API_URL = "http://192.168.15.9:80/api"
 STACK_NAME = "cob-service"
-SERVICE_NAME = "backend.yaml"
+SERVICE_NAME = "03-backend.yaml"
 FULL_SERVICE_NAME = f"{STACK_NAME}_{SERVICE_NAME}"
